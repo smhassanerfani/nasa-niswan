93a281221c799f89359a8bdc91fa02cc90295ef9
smhassanerfani <serfani@email.sc.edu>
2024-05-10 23:35:30 -0400

    feat(launcher): add launcher shell script

---
+++ launcher.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# Change to the directory where your Python script is located
+cd /home/serfani/projects/nasa-niswan/
+
+# Activate the virtual environment
+source /home/serfani/projects/nasa-niswan/.venv/bin/activate
+
+MODEL=E33OMA-10
+
+# Run the Python script with arguments
+python train.py \
+    --model $MODEL\
+    --species seasalt \
+    --learning-rate 1.0E-04 \
+    --dataset E33OMA \
+    --in-channels 5 \
+    --num-epochs 50 \
+    --input-size 256 \
+    --batch-size 4 \
+    --num-workers 1 \
+    --scheduler-step 10 \
+    --betas 0.5 0.999 \
+    --snapshot-dir /home/serfani/serfani_data0/snapshots/$MODEL \
+    --restore-from /home/serfani/serfani_data0/snapshots/$MODEL \
+    # --transform \
+    # --use-checkpoint \
+    > /home/serfani/serfani_data0/snapshots/$MODEL/run_print.txt
+
+# Deactivate the virtual environment
+deactivate
