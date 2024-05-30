3c50abab06cf751db7f9ab276a0c860c5b9a5456
smhassanerfani <serfani@email.sc.edu>
2024-05-30 15:37:21 -0400

    feat(launcher): add configs for ConvLSTM model

--- launcher.sh
+++ launcher.sh
@@ -7,18 +7,22 @@ cd /home/serfani/projects/nasa-niswan/
 source /home/serfani/projects/nasa-niswan/.venv/bin/activate
 
 DATEDIR=$(date +"%Y%m%d-%H%M");
-MODEL=PIX2PIX-E33OMA90D-BCB-$DATEDIR; # [UNet/PIX2PIX]-[E33OMA/E33OMA90D]-[BCB/SST/CLY]-$DATEDIR;
+MODEL=LSTM-3C-E33OMA-CRNN-BCB-$DATEDIR; # [UNet/PIX2PIX/LSTM-<NUM-CELLS>C]-[E33OMA/E33OMA90D]-[5C/6C]-[BCB/SST/CLY]-$DATEDIR;
 
 # Run the Python script with arguments
 python train.py \
     --model $MODEL\
     --species bcb \
-    --learning-rate 1.0E-04 \
-    --dataset E33OMA90D \
+    --learning-rate 1.0E-03 \
+    --dataset E33OMA-CRNN \
     --in-channels 5 \
+    --hidden-channels 10 \
+    --kernel-size 3 \
+    --num-layers 3 \
+    --sequence-length 48 \
     --num-epochs 50 \
     --input-size 256 \
-    --batch-size 4 \
+    --batch-size 16 \
     --num-workers 1 \
     --scheduler-step 10 \
     --betas 0.5 0.999 \
