0dc8550999c16bc6885ab5162b3de36e152d07ef
smhassanerfani <serfani@email.sc.edu>
2024-06-06 12:22:32 -0400

    feat(launcher): modify `--input-size` to recieve `HxW` separately

--- launcher.sh
+++ launcher.sh
@@ -7,24 +7,24 @@ cd /home/serfani/projects/nasa-niswan/
 source /home/serfani/projects/nasa-niswan/.venv/bin/activate
 
 DATEDIR=$(date +"%Y%m%d-%H%M");
-MODEL=LSTM-3C-E33OMA-CRNN-BCB-$DATEDIR; # [UNet/PIX2PIX/LSTM-<NUM-CELLS>C]-[E33OMA/E33OMA90D]-[5C/6C]-[BCB/SST/CLY]-$DATEDIR;
+MODEL=LSTM-3C16-E33OMA90D-CRNN-BCB-$DATEDIR; # [UNet/PIX2PIX/LSTM-<NUM-CELLS>C-<NUM-STATES>]-[E33OMA/E33OMA90D]-[5C/6C]-[BCB/SST/CLY]-$DATEDIR;
 
 # Run the Python script with arguments
 python train.py \
     --model $MODEL\
     --species bcb \
     --learning-rate 1.0E-03 \
-    --dataset E33OMA-CRNN \
+    --dataset E33OMA90D-CRNN \
     --in-channels 5 \
-    --hidden-channels 10 \
+    --hidden-channels 16 \
     --kernel-size 3 \
     --num-layers 3 \
     --sequence-length 48 \
-    --num-epochs 50 \
-    --input-size 256 \
+    --num-epochs 100 \
+    --input-size 100 154 \
     --batch-size 16 \
     --num-workers 1 \
-    --scheduler-step 10 \
+    --scheduler-step 20 \
     --betas 0.5 0.999 \
     --snapshot-dir /home/serfani/serfani_data0/snapshots/$MODEL \
     --restore-from /home/serfani/serfani_data0/snapshots/$MODEL \
