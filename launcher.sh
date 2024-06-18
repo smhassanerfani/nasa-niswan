143a32b95f73b21f4178c3404f45df3f92dce01b
smhassanerfani <serfani@email.sc.edu>
2024-06-18 14:42:24 -0400

    refactor(launcher): improve naming convention to better represent trained models

--- launcher.sh
+++ launcher.sh
@@ -7,24 +7,24 @@ cd /home/serfani/projects/nasa-niswan/
 source /home/serfani/projects/nasa-niswan/.venv/bin/activate
 
 DATEDIR=$(date +"%Y%m%d-%H%M");
-MODEL=LSTM-3C16-E33OMA90D-CRNN-BCB-$DATEDIR; # [UNet/PIX2PIX/LSTM-<NUM-CELLS>C-<NUM-STATES>]-[E33OMA/E33OMA90D]-[5C/6C]-[BCB/SST/CLY]-$DATEDIR;
+MODEL=LSTM-64K5.32K3.16K3-E33OMA90D-BCB-$DATEDIR; # [UNet/PIX2PIX/LSTM-<HIDDEN-STATES>K<KERNEL-SIZE>]-[E33OMA/E33OMA90D]-[5C/6C]-[BCB/SST/CLY]-$DATEDIR;
 
 # Run the Python script with arguments
 python train.py \
     --model $MODEL\
     --species bcb \
     --learning-rate 1.0E-03 \
-    --dataset E33OMA90D-CRNN \
+    --dataset E33OMA90D \
     --in-channels 5 \
-    --hidden-channels 16 \
-    --kernel-size 3 \
+    --hidden-channels 64 32 16 \
+    --kernel-size 5 3 3 \
     --num-layers 3 \
     --sequence-length 48 \
-    --num-epochs 100 \
+    --num-epochs 50 \
     --input-size 100 154 \
-    --batch-size 16 \
+    --batch-size 8 \
     --num-workers 1 \
-    --scheduler-step 20 \
+    --scheduler-step 25 \
     --betas 0.5 0.999 \
     --snapshot-dir /home/serfani/serfani_data0/snapshots/$MODEL \
     --restore-from /home/serfani/serfani_data0/snapshots/$MODEL \
