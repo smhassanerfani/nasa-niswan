264a423c0a177b7eaf22ab85d4d3387b5b395c7a
smhassanerfani <serfani@email.sc.edu>
2024-07-19 12:48:42 -0400

    refactor(launcher): modify the reading/saving path from data0 to data1

--- launcher.sh
+++ launcher.sh
@@ -7,7 +7,7 @@ cd /home/serfani/projects/nasa-niswan/
 source /home/serfani/projects/nasa-niswan/.venv/bin/activate
 
 DATEDIR=$(date +"%Y%m%d-%H%M");
-MODEL=LSTM-64K5.32K3.16K3-E33OMA-BCB-$DATEDIR; # [UNet/PIX2PIX/LSTM-<HIDDEN-STATES>K<KERNEL-SIZE>]-[E33OMA/E33OMA90D]-[5C/6C]-[BCB/SST/CLY]-$DATEDIR;
+MODEL=LSTM-64K5.32K3.16K3-E33OMA-8C-BCB-$DATEDIR; # [UNet/PIX2PIX/LSTM-<HIDDEN-STATES>K<KERNEL-SIZE>]-[E33OMA/E33OMA90D]-[5C/6C]-[BCB/SST/CLY]-$DATEDIR;
 
 # Run the Python script with arguments
 python train.py \
@@ -15,7 +15,7 @@ python train.py \
     --species bcb \
     --learning-rate 1.0E-03 \
     --dataset E33OMA \
-    --in-channels 22 \
+    --in-channels 8 \
     --hidden-channels 64 32 16 \
     --kernel-size 5 3 3 \
     --num-layers 3 \
@@ -26,8 +26,8 @@ python train.py \
     --num-workers 1 \
     --scheduler-config 10 0.9 \
     --betas 0.5 0.999 \
-    --snapshot-dir /home/serfani/serfani_data0/snapshots/$MODEL \
-    --restore-from /home/serfani/serfani_data0/snapshots/$MODEL \
+    --snapshot-dir /home/serfani/serfani_data1/snapshots/$MODEL \
+    --restore-from /home/serfani/serfani_data1/snapshots/$MODEL \
     # --transform \
     # --use-checkpoint \
 
