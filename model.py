afd0ad29acea90e4b81ec515d4a7379df1a75f5a
smhassanerfani <serfani@email.sc.edu>
2025-03-12 15:32:27 -0400

    feat(model): add new config for optimizer, scheduler, and loss funcs

--- model.py
+++ model.py
@@ -5,6 +5,7 @@ import torch.nn as nn
 import pytorch_lightning as pl
 
 from src import ConvLSTM, SimVP_Model, Conv3D, SAConvLSTM 
+from utils import RMSELoss
 
 class STMLightning(pl.LightningModule):
     def __init__(self, model_args, data_args):
@@ -13,10 +14,14 @@ class STMLightning(pl.LightningModule):
         self.save_hyperparameters()
         self.model_args = model_args
         self.data_args = data_args
+        image_size=(self.data_args['size'][0] + 2*self.data_args['padding'][0], 
+                    self.data_args['size'][1] + 2*self.data_args['padding'][1]
+            )   
 
         if 'ConvLSTM' == self.model_args['model_name']:
 
             self.model = ConvLSTM(
+                image_size=image_size,
                 in_channels=self.model_args['in_channels'], 
                 out_channels=self.model_args['out_channels'], 
                 hidden_channels=self.model_args['encoder_channels'],
@@ -57,8 +62,13 @@ class STMLightning(pl.LightningModule):
             )
 
 
-        self.loss1 = nn.MSELoss()
-        self.loss2 = nn.L1Loss()
+        # Initialize loss functions based on config
+        loss_dict = {
+            'MSE': nn.MSELoss(),
+            'MAE': nn.L1Loss(),
+            'RMSE': RMSELoss(dataset=data_args['data_name'])
+        }
+        self.losses = [loss_dict[loss] for loss in self.model_args['loss']]
 
     def forward(self, x):
         return self.model(x)
@@ -70,9 +80,7 @@ class STMLightning(pl.LightningModule):
                       self.data_args['padding'][0]:self.data_args['padding'][0]+self.data_args['size'][0], 
                       self.data_args['padding'][1]:self.data_args['padding'][1]+self.data_args['size'][1]]
         
-        loss1 = self.loss1(y_hat, y)
-        loss2 = self.loss2(y_hat, y)
-        total_loss = (loss1 + loss2) / 2
+        total_loss = sum(loss(y_hat, y) for loss in self.losses) / len(self.losses)
 
         self.log('train_loss', total_loss, on_step=True, on_epoch=True, prog_bar=True, logger=True)
         return total_loss
@@ -84,38 +92,61 @@ class STMLightning(pl.LightningModule):
                       self.data_args['padding'][0]:self.data_args['padding'][0]+self.data_args['size'][0], 
                       self.data_args['padding'][1]:self.data_args['padding'][1]+self.data_args['size'][1]]
 
-        loss1 = self.loss1(y_hat, y)
-        loss2 = self.loss2(y_hat, y)
-
-        total_loss = (loss1  + loss2) / 2
+        total_loss = sum(loss(y_hat, y) for loss in self.losses) / len(self.losses)
 
         self.log('val_loss', total_loss, on_step=True, on_epoch=True, prog_bar=True, logger=True)
         return total_loss
 
     def configure_optimizers(self):
-        optimizer = torch.optim.Adam(
-            self.model.parameters(), 
-            lr=self.model_args['learning_rate'],
-            # weight_decay=self.model_args['weight_decay']
-            )
+
+        optimizer_params = {
+            'params': self.model.parameters(),
+            'lr': self.model_args['learning_rate'],
+            'weight_decay': self.model_args['weight_decay']
+        }
+        
+        if self.model_args['optimizer'] == 'Adam':
+            optimizer = torch.optim.Adam(**optimizer_params)
+        
+        elif self.model_args['optimizer'] == 'AdamW':
+            optimizer = torch.optim.AdamW(**optimizer_params)
 
         # Define the scheduler
-        scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=10, gamma=0.5, last_epoch=-1)
-        # scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer, T_max=self.model_args['epochs'], eta_min=0)
-        # scheduler = torch.optim.lr_scheduler.OneCycleLR(
-        #     optimizer, 
-        #     max_lr=self.model_args['learning_rate']*10,
-        #     steps_per_epoch=self.data_args['steps_per_epoch'], 
-        #     epochs=self.model_args['epochs']
-        #     )
-        
+        if self.model_args['scheduler'] == 'StepLR':
+            scheduler = torch.optim.lr_scheduler.StepLR(
+                optimizer, 
+                **self.model_args['scheduler_params']
+            )
+            interval = 'epoch'
+        elif self.model_args['scheduler'] == 'ReduceLROnPlateau':
+            scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(
+                optimizer, 
+                **self.model_args['scheduler_params']
+            )
+            interval = 'epoch'
+        elif self.model_args['scheduler'] == 'CosineAnnealingLR':
+            scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(
+                optimizer, 
+                **self.model_args['scheduler_params']
+            )
+            interval = 'epoch'
+        elif self.model_args['scheduler'] == 'OneCycleLR':
+            scheduler = torch.optim.lr_scheduler.OneCycleLR(
+                optimizer, 
+                **self.model_args['scheduler_params']
+            )
+            interval = 'step'
+        else:
+            scheduler = None
+            interval = 'epoch'
+
         # Return both optimizer and scheduler
         return {
             "optimizer": optimizer,
             "lr_scheduler": {
                 "scheduler": scheduler,
                 "monitor": "val_loss",  # Metric to monitor
-                "interval": "step",    # Frequency of the scheduler
+                "interval": interval,   # Frequency of the scheduler
                 "frequency": 1,         # How often to check the monitor
                 "strict": True,         # Enforce the availability of the monitored metric
             },
