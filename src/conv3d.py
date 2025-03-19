a29f814d3826d7011a288a8d67be83bed20f2a56
smhassanerfani <serfani@email.sc.edu>
2025-03-19 11:37:02 -0400

    feat(conv3d): set the model for 32 sequence length with different readout layer

--- src/conv3d.py
+++ src/conv3d.py
@@ -15,7 +15,7 @@ class GLU(nn.Module):
         self.sigmoid = nn.Sigmoid()
 
     def forward(self, x):
-        x = F.pad(x, (0, 0, 0, 0, self.causal_padding, 0, 0, 0, 0, 0)) # Pad the depth dimension
+        # x = F.pad(x, (0, 0, 0, 0, self.causal_padding, 0, 0, 0, 0, 0)) # Pad the depth dimension
 
         gates = self.conv(x)
         x, outgate  = torch.split(gates, self.out_channels, dim=1)
@@ -29,98 +29,101 @@ class Conv3D(nn.Module):
 
         # Explicitly define each Conv3D layer with correct dilation
         self.conv1 = GLU(
-            64,
-            64,
+            256,
+            256,
             kernel_size=kernel_size,
             padding=padding,
             dilation=(1, 1, 1)
         )
 
         self.conv2 = GLU(
-            64,
-            32,
+            256,
+            128,
             kernel_size=kernel_size,
             padding=padding,
             dilation=(2, 1, 1)
         )
 
         self.conv3 = GLU(
-            32,
-            16,
+            128,
+            128,
             kernel_size=kernel_size,
             padding=padding,
             dilation=(4, 1, 1)
         )
 
         self.conv4 = GLU(
-            16,
-            8,
+            128,
+            64,
             kernel_size=kernel_size,
             padding=padding,
             dilation=(8, 1, 1)
         )
 
         self.conv5 = GLU(
-            8,
-            4,
+            64,
+            22,
             kernel_size=kernel_size,
             padding=padding,
             dilation=(16, 1, 1)
         )
 
-        self.conv6 = GLU(
-            4,
-            1,
-            kernel_size=kernel_size,
-            padding=padding,
-            dilation=(32, 1, 1)
-        )
+        # self.conv6 = GLU(
+        #     64,
+        #     out_channels,
+        #     kernel_size=kernel_size,
+        #     padding=padding,
+        #     dilation=(32, 1, 1)
+        # )
        
         self.encoder = Encoder(in_channels, 5)
         # self.decoder = Decoder(1, 5)
         
         # self.readout_ftr = nn.Conv2d(64, out_channels, kernel_size=1, stride=1)
-        self.readout_tmp = nn.Conv2d(48, out_channels, kernel_size=1, stride=1)
+        # self.readout_tmp = nn.Conv2d(48, 1, kernel_size=1, stride=1)
+        self.readout = nn.Conv2d(22, out_channels, kernel_size=1, stride=1)
 
-    def forward(self, x):
-        B, T, C, H, W = x.size()
-        # H_enc, W_enc = H//2, W//2
+    def forward(self, x, e):
+        B, T, C, L, H, W = x.size() # torch.Size([8, 48, 4, 22, 90, 144]) 
 
-        x = x.view(B*T, C, H, W)
-        x, _ = self.encoder(x) # BxT, F, H//2, W//2
+        x = x.view(B*T, C*L, H, W) # torch.Size([384, 88, 90, 144])
+        e = e.view(B*T, 1, H, W)   # torch.Size([384, 1, 90, 144])
+        x = torch.concat((x, e), dim=1) # torch.Size([384, 89, 90, 144])
 
-        x = x.view(B, T, 64, H, W)
+        x, _ = self.encoder(x) # BxT, 256, H, W
+
+        x = x.view(B, T, 256, H, W)
         x = x.permute(0, 2, 1, 3, 4)  # BxTxFxHxW -> BxFxTxHxW
 
-        x = self.conv1(x) # 128
-        x = self.conv2(x) # 64
-        x = self.conv3(x) # 32
-        x = self.conv4(x) # 16
-        x = self.conv5(x) # 8
-        x = self.conv6(x) # 1 -> Bx1x1xHxW
+        x = self.conv1(x) 
+        x = self.conv2(x) 
+        x = self.conv3(x) 
+        x = self.conv4(x) 
+        x = self.conv5(x) 
+        # x = self.conv6(x) # Bx22x48xHxW
+        x = x.view(B, L, H, W)
+        x = self.readout(x)
 
-        # x = x.permute(0, 2, 1, 3, 4)  # BxFxCxHxW -> BxTxFxHxW
-        # x = x.contiguous().view(B*T, 1, H_enc, W_enc)
+        # x = x.view(B*L, T, H, W)
+        # x = self.readout_tmp(x)
+        x = x.view(B, 1, 1, L, H, W) # torch.Size([8, 1, 1, 22, 90, 144])
 
-        # x = self.decoder(x) # BxT, 1, 32, 32
-
-        x = x.view(B, T, H, W)
-        x = self.readout_tmp(x)
-
-        return x.unsqueeze(2)  # BxT, 1, 1, 32, 32
+        return x
 
 class Encoder(nn.Module):
 
     def __init__(self, in_channels, kernel_size):
         super(Encoder, self).__init__()
-        self.conv1 = ConvLayer(in_channels,  64, kernel_size, stride=1)
-        self.conv2 = ConvLayer(64, 64, kernel_size, stride=1)
-        self.conv3 = ConvLayer(64, 64, kernel_size, stride=1)
-        self.conv4 = ConvLayer(64, 64, kernel_size, stride=1)
+        self.conv1 = ConvLayer(in_channels,  128, kernel_size, stride=1)
+        self.conv2 = ConvLayer(128, 128, kernel_size, stride=1)
+        self.conv3 = ConvLayer(128, 256, kernel_size, stride=1)
+        self.conv4 = ConvLayer(256, 256, kernel_size, stride=1)
     
     def forward(self, x):  # BxT, 5, 160, 160
         enc = self.conv1(x)
         x = self.conv2(enc)
+        x = self.conv3(x)
+        x = self.conv4(x)
 
         return x, enc
 
@@ -178,9 +181,10 @@ class ConvTransposeLayer(nn.Module):
     
 def main():
 
-    x = torch.randn((8, 48, 5, 90, 144))
-    model = Conv3D(in_channels=5, out_channels=1, kernel_size=(2, 3, 3))
-    y = model(x)
+    x = torch.randn((2, 32, 4, 22, 90, 144))
+    e = torch.randn((2, 32, 1, 1, 90, 144))
+    model = Conv3D(in_channels=89, out_channels=22, kernel_size=(2, 3, 3))
+    y = model(x, e)
     print(y.size())
     print(model)
 
